5104738720abac3db4dfaed95c840499d9291cc2
forestmeichtry <fmeichtry@gmail.com>
2023-05-29 15:05:26 -0700

    -Add popup task form with styling

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ addNewProjectButton.classList.add('addNewProjectButton');
 addNewProjectButton.textContent = 'Add Project';
 sideNavBar.appendChild(addNewProjectButton);
 
+const addTaskPopup = document.createElement('div');
+createTaskForm();
+
 addProject('Work');
 addProject('Home');
 
@@ -48,7 +51,7 @@ addTaskButton.textContent = "Add Task";
 pageHeader.appendChild(addTaskButton);
 
 addTaskButton.addEventListener('click', () => {
-    addTask('Complete Todo List', 'Finish TOP coding project', '10/10/2023', 'High', 'Coding Projects');
+    addTaskPopup.classList.toggle('active');
 });
 
 if (window.screen.width <= 1024) {
@@ -60,3 +63,131 @@ if (window.screen.width <= 1024) {
         document.body.classList.toggle('mobileMenu');
     });
 }
+
+function createTaskForm() {
+    addTaskPopup.classList.add('addTaskPopup');
+    document.body.appendChild(addTaskPopup);
+
+    const taskFieldset = document.createElement('fieldset');
+    addTaskPopup.appendChild(taskFieldset);
+    const legend = document.createElement('legend');
+    legend.textContent = 'New Task';
+    taskFieldset.appendChild(legend);
+
+    const formList = document.createElement('form');
+    formList.classList.add('formList');
+    taskFieldset.appendChild(formList);
+    const labelOne = document.createElement('label');
+    formList.appendChild(labelOne);
+    const nameInput = document.createElement('input');
+    nameInput.required = 'true';
+    nameInput.autocomplete = 'off';
+    formList.appendChild(nameInput);
+    labelOne.htmlFor = 'Task Name';
+    labelOne.textContent = 'Task Name:';
+    nameInput.type = 'text';
+    nameInput.id = 'Task Name';
+    nameInput.name = 'task_name';
+    const labelTwo = document.createElement('label');
+    formList.appendChild(labelTwo);
+    const dateInput = document.createElement('input');
+    formList.appendChild(dateInput);
+    labelTwo.htmlFor = 'Task Due Date';
+    labelTwo.textContent = 'Due Date:';
+    dateInput.type = 'date';
+    dateInput.id = 'Task Due Date';
+    nameInput.name = 'due_date';
+    const labelThree = document.createElement('label');
+    formList.appendChild(labelThree);
+    const radioWrap = document.createElement('div');
+    radioWrap.classList.add('radioWrap');
+    formList.appendChild(radioWrap);
+    const radioLow = document.createElement('input');
+    radioWrap.appendChild(radioLow);
+    const labelLow = document.createElement('label');
+    radioWrap.appendChild(labelLow);
+    radioLow.type = 'radio';
+    radioLow.id = 'importanceLow';
+    radioLow.name = 'importance';
+    radioLow.value = 'Low';
+    labelLow.htmlFor = 'importanceLow';
+    labelLow.textContent = 'Low';
+    labelThree.textContent = 'Importance:';
+    const radioMed = document.createElement('input');
+    radioWrap.appendChild(radioMed);
+    const labelMed = document.createElement('label');
+    radioWrap.appendChild(labelMed);
+    radioMed.type = 'radio';
+    radioMed.id = 'importanceMed';
+    radioMed.name = 'importance';
+    radioMed.value = 'Medium';
+    radioMed.checked = true;
+    labelMed.htmlFor = 'importanceMedium';
+    labelMed.textContent = 'Medium';
+    const radioHigh = document.createElement('input');
+    radioWrap.appendChild(radioHigh);
+    const labelHigh = document.createElement('label');
+    radioWrap.appendChild(labelHigh);
+    radioHigh.type = 'radio';
+    radioHigh.id = 'importanceHigh';
+    radioHigh.name = 'importance';
+    radioHigh.value = 'High';
+    labelHigh.htmlFor = 'importanceHigh';
+    labelHigh.textContent = 'High';
+    const labelFour = document.createElement('label');
+    formList.appendChild(labelFour);
+    const descriptionInput = document.createElement('textarea');
+    descriptionInput.rows = '8';
+    formList.appendChild(descriptionInput);
+    labelFour.htmlFor = 'Task Description';
+    labelFour.textContent = 'Task Description:';
+    descriptionInput.id = 'Task Description';
+    descriptionInput.name = 'description';
+    const formButtonWrap = document.createElement('div');
+    formButtonWrap.classList.add('formButtonWrap');
+    formList.appendChild(formButtonWrap);
+
+    const cancelButton = document.createElement('button');
+    cancelButton.textContent = 'Cancel';
+    formButtonWrap.appendChild(cancelButton);
+    cancelButton.addEventListener('click', () => {
+        addTaskPopup.classList.remove('active');
+    });
+
+    const submitButton = document.createElement('button');
+    submitButton.textContent = 'Submit';
+    submitButton.type = 'submit';
+    formButtonWrap.appendChild(submitButton);
+
+    formList.addEventListener('submit', function(event) {
+        event.preventDefault();
+        addTaskPopup.classList.remove('active');
+
+        let dateValue = null;
+        if (dateInput.value === '') {
+            dateValue = 'No Due Date';
+        } else {
+            dateValue = dateInput.value;
+        };
+
+        let checkedRadio = null;
+        let radioElements = document.getElementsByName('importance');
+        for (let i = 0; i < 3; i++) {
+            if (radioElements[i].checked) {
+                checkedRadio = radioElements[i].value;
+            };
+        };
+
+        let descriptionValue = null;
+        if (descriptionInput.value === '') {
+            descriptionValue = 'None';
+        } else {
+            descriptionValue = descriptionInput.value;
+        };
+
+        addTask(nameInput.value, descriptionValue, dateValue, checkedRadio, 'Coding Projects');
+
+        formList.reset();
+        radioMed.checked = true;
+    });
+};
